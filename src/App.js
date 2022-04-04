88656b43795cf73a21b5ad82aa30568d13bc51c3
Jorian72 <Jorian27@outlook.com>
2022-04-04 21:03:56 +0200

    API ingeladen en CSS toegevoegd
    Api ingeladen en CSS toegevoegd. Ook de foto's ingeladen.

--- src/App.js
+++ src/App.js
@@ -1,8 +1,35 @@
 import React from 'react';
-import characters from './views/characters';
-function App(){
+import "./App.css";
 
+export default class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { item: [] };
+  }
+ 
+  componentDidMount() {
+    //api call
+    fetch("http://hp-api.herokuapp.com/api/characters")
+      .then((resp) => resp.json())
+      .then((resp) => this.setState({ item: resp }));
+  }
+  render() {
+
+    return (
+      <div className='grid-container'>
+          {this.state.item.map((item) => (
+           <ol key = { item.id } >
+          <li> { item.name }</li>
+           <li>Gender: {item.gender}</li> 
+           <li>{item.house} </li>
+           <li>Date of birth: {item.dateOfBirth} </li>
+          <li>{item.ancestry} </li>
+            <li>Patronus: {item.patronus} </li>
+             <li>Actor: {item.actor}</li>
+             <img className='image' src={item.image}></img> 
+             </ol>
+          ))}
+      </div>
+    );
+  }
 }
-
-
-export default App;
