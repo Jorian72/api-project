ce46c1cfc1d79001065f276916ddc5b437ea5484
JessicaW23 <woudsje@gmail.com>
2022-04-06 21:51:36 +0200

    Codereview door Jessica
    In de code heb ik comments gezet. In het algemeen: de folder components met Menu.js en Menu.scss kun je verwijderen. In App.css staat code die niet uitgevoerd wordt.

--- src/App.js
+++ src/App.js
@@ -1,55 +1,81 @@
-import React from 'react';
+import React from "react";
 import "./App.css";
 export default class App extends React.Component {
   constructor(props) {
     super(props);
     this.state = { item: [] };
   }
-  
- 
-  houseFilter = house => () => {
+
+  houseFilter = (house) => () => {
     console.log(house);
     fetch(`http://hp-api.herokuapp.com/api/characters/house/${house}`)
       .then((resp) => resp.json())
       .then((resp) => this.setState({ item: resp }));
-  }
+  };
   componentDidMount() {
     //api call
     fetch("http://hp-api.herokuapp.com/api/characters")
       .then((resp) => resp.json())
       .then((resp) => this.setState({ item: resp }));
   }
-  render() {
-    
-    return (
-      
-      <div className='nav'>
-      <ul>
-          <a value="Gryffindor" onClick={this.houseFilter("Gryffindor")}><img src={require('.//images/Gryffindor.png')} alt='Gryffindor'width="100" height="100"/></a>
-          <a value="Slytherin" onClick={this.houseFilter("Slytherin")}><img src={require('.//images/slytherin.png')} alt='Slytherin' width="100" height="100"/></a>
-          <a value="Hufflepuff" onClick={this.houseFilter("Hufflepuff")}><img src={require('.//images/Hufflepuff.png')} alt='Hufflepuff' width="100" height="100"/></a>
-          <a value="Ravenclaw" onClick={this.houseFilter("Ravenclaw")}><img src= {require('.//images/ravenclaw.png')}alt='Ravenclaw' width="100" height="100"/></a>
-            </ul>
- 
-      <div className='grid-container'>
-    
 
+  //Ik zou de opmaak van afbeeldingen voor de overzichtelijkheid in app.css doen.
+  //In regel 67 zou ik voor de volledigheid ook Name: toevoegen. Hetzelfde geldt voor regel 69 em 71.
+  render() {
+    return (
+      <div className="nav">
+        <ul>
+          <a value="Gryffindor" onClick={this.houseFilter("Gryffindor")}>
+            <img
+              src={require(".//images/Gryffindor.png")}
+              alt="Gryffindor"
+              width="100"
+              height="100"
+            />
+          </a>
+          <a value="Slytherin" onClick={this.houseFilter("Slytherin")}>
+            <img
+              src={require(".//images/slytherin.png")}
+              alt="Slytherin"
+              width="100"
+              height="100"
+            />
+          </a>
+          <a value="Hufflepuff" onClick={this.houseFilter("Hufflepuff")}>
+            <img
+              src={require(".//images/Hufflepuff.png")}
+              alt="Hufflepuff"
+              width="100"
+              height="100"
+            />
+          </a>
+          <a value="Ravenclaw" onClick={this.houseFilter("Ravenclaw")}>
+            <img
+              src={require(".//images/ravenclaw.png")}
+              alt="Ravenclaw"
+              width="100"
+              height="100"
+            />
+          </a>
+        </ul>
+
+        <div className="grid-container">
           {this.state.item.map((item) => (
-           <ol key = { item.id } >
-             <img className='image' src={item.image}></img> 
-             <div className='text'>
-          <li> { item.name }</li>
-           <li>Gender: {item.gender}</li> 
-           <li>{item.house} </li>
-           <li>Date of birth: {item.dateOfBirth} </li>
-          <li>{item.ancestry} </li>
-            <li>Patronus: {item.patronus} </li>
-             <li>Actor: {item.actor}</li>
-             </div>
-             </ol>
-             
-          ))}</div>
-      </div> 
+            <ol key={item.id}>
+              <img className="image" src={item.image}></img>
+              <div className="text">
+                <li> {item.name}</li>
+                <li>Gender: {item.gender}</li>
+                <li>{item.house} </li>
+                <li>Date of birth: {item.dateOfBirth} </li>
+                <li>{item.ancestry} </li>
+                <li>Patronus: {item.patronus} </li>
+                <li>Actor: {item.actor}</li>
+              </div>
+            </ol>
+          ))}
+        </div>
+      </div>
     );
   }
 }
---
+++ src/components/Menu.js
@@ -0,0 +1,1 @@
+//Dit bestand kun je verwijderen, omdat er niks in staat en je linkt nergens naar dit bestand. Ook kun je de folder components verwijderen.
